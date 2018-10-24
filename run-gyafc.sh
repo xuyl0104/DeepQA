46397d8bbb21147c3a147927bb476404c19b32ea
xuyl0104 <xuyl0104@gmail.com>
2018-10-23 23:28:35 -0400

    modify run-hyafc,sh

--- run-gyafc.sh
+++ run-gyafc.sh
@@ -1,99 +1,81 @@
 #!/bin/sh
-# fix number of epochs (50, 100) and learning rate (0.01), change batch size
+# fix number of epochs (50) and initial learning rate (0.01, decrease gradually), change batch size
 rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.01
+python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=250 --learningRate=0.01
 
 rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.01
+python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=350 --learningRate=0.01
 
 rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.01
+python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=450 --learningRate=0.01
+
+
+
+# fix number of epochs(50) and batch size (250), change initial learning rate
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=250 --learningRate=0.005
+
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=250 --learningRate=0.0075
 
 rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=100 --learningRate=0.01
+python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=250 --learningRate=0.01
+
+rm ./save/model/*
+python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=250 --learningRate=0.0125
+
+rm ./save/model/*
+python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=250 --learningRate=0.015
+
 
 
 
 # rm ./save/model/*
-# python ./main.py --keepAll --corpus gyafc --numEpochs=100 --batchSize=400 --learningRate=0.01
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.005
 
 # rm ./save/model/*
-# python ./main.py --keepAll --corpus gyafc --numEpochs=100 --batchSize=300 --learningRate=0.01
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.0075
 
 # rm ./save/model/*
-# python ./main.py --keepAll --corpus gyafc --numEpochs=100 --batchSize=200 --learningRate=0.01
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.01
 
 # rm ./save/model/*
-# python ./main.py --keepAll --corpus gyafc --numEpochs=100 --batchSize=100 --learningRate=0.01
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.0125
 
-
-
-# fix number of epochs and batch size (100, 200, 300, 400), change learning rate
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=100 --learningRate=0.005
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=100 --learningRate=0.0075
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=100 --learningRate=0.01
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=100 --learningRate=0.0125
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=100 --learningRate=0.015
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.015
 
 
 
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.005
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.005
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.0075
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.0075
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.01
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.01
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.0125
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.0125
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=200 --learningRate=0.015
-
-
-
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.005
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.0075
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.01
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.0125
-
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.015
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=300 --learningRate=0.015
 
 
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.005
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.005
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.0075
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.0075
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.01
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.01
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.0125
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.0125
 
-rm ./save/model/*
-python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.015
-
+# rm ./save/model/*
+# python ./main.py --keepAll --corpus gyafc --numEpochs=50 --batchSize=400 --learningRate=0.015
